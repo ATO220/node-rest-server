39828795169fe98171fa453473fff9355235caff
ATO220 <serranolaureano.220@gmail.com>
2019-05-05 10:00:35 -0300

    fix config

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //  DataBase
 //======================================= 
 let urlDB;
-if(process.env.NODE_ENV === 'prod'){
-    urlDB = 'mongodb://RESTuser:restuser123@ds135704.mlab.com:35704/rest-server-s9';
-}else{
+if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb://RESTuser:restuser123@ds135704.mlab.com:35704/rest-server-s9';
+    
 }
 
 process.env.URL_DB = urlDB;
