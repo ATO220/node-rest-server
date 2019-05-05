7c50b1500ff06969768bd4be3735ab2677edbde7
ATO220 <serranolaureano.220@gmail.com>
2019-05-05 10:19:54 -0300

    Set variable environment for uri of DB

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb://RESTuser:restuser123@ds135704.mlab.com:35704/rest-server-s9';
+    urlDB = process.env.URI_DB;
     
 }
 
